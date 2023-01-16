4fbbd538f5a0681ab22e2099c257693883ddf413
[Fernando Rico] <[luisf.ricoo@gmail.com]>
2023-01-15 21:52:46 -0500

    Ajuste Deploy Docker solo pip

--- dagster_template/repository_test.py
+++ dagster_template/repository_test.py
@@ -1,8 +1,12 @@
 from dagster import repository
-from dagster_template.assets.asset_one import *
+from assets.asset_one import *
 
 @repository(name='data_gsheets')
 def repo_one():
     return [
-        get_dataframe, save_gsheet, save_local_csv, all_assets_job, asset2_job
+        get_dataframe, 
+        save_gsheet, 
+        save_local_csv, 
+        all_assets_job, 
+        asset2_job
     ]
